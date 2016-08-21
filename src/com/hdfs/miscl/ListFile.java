fd45d134cf4143f0ad44bf47cc2a981c7b2f035b
shwetav1311 <cool.shwetav@gmail.com>
2016-08-22 00:58:06 +0530

    implemented wild card

--- src/com/hdfs/miscl/ListFile.java
+++ src/com/hdfs/miscl/ListFile.java
@@ -1,4 +1,17 @@
 package com.hdfs.miscl;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.hdfs.miscl.Hdfs.ListFilesRequest;
+import com.hdfs.miscl.Hdfs.ListFilesResponse;
+import com.hdfs.namenode.INameNode;
+
 /**
  * 
  * @author shweta
@@ -6,4 +19,47 @@ package com.hdfs.miscl;
  */
 public class ListFile {
 
+	public String wildCard;
+	
+	public ListFile(String wild)
+	{
+		this.wildCard = wild;
+	}
+
+
+	public List<String> list()
+	{
+		ListFilesRequest.Builder listFileReqObj = ListFilesRequest.newBuilder();
+		
+		
+		listFileReqObj.setDirName(wildCard);
+		
+		Registry registry;
+		try {
+			registry = LocateRegistry.getRegistry(Constants.NAME_NODE_IP,Registry.REGISTRY_PORT);
+			INameNode nameStub;
+			nameStub=(INameNode) registry.lookup(Constants.NAME_NODE);
+			
+			byte[] responseArray = nameStub.list(listFileReqObj.build().toByteArray());
+			try {
+				ListFilesResponse listFileResObj = ListFilesResponse.parseFrom(responseArray);
+				List<String> fileNames = listFileResObj.getFileNamesList();
+				
+				return fileNames;
+				
+				
+			} catch (InvalidProtocolBufferException e) {
+				 
+				System.out.println("InvalidProtocolBufferException caught in callListBlocks: ClientDriverClass");
+			}
+			
+		} catch (RemoteException | NotBoundException e) {
+			
+			System.out.println("Exception caught in callListBlocks: ClientDriverClass");
+			
+		}
+		
+		
+		return new ArrayList<String>();
+	}
 }
