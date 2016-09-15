4444f04b2b738868c45ead9ae05f432467fc9ed4
Sheshadri-1992 <sheshadri.madhu@gmail.com>
2016-09-15 23:11:41 +0530

    HBase constants

---
+++ src/com/hbase/miscl/HBaseConstants.java
@@ -0,0 +1,8 @@
+package com.hbase.miscl;
+
+public final class HBaseConstants {
+	
+	public static final String  RS_DRIVER = "RegionServer";  
+	public static final String  RS_DRIVER_IP = "127.0.0.1";	
+
+}
