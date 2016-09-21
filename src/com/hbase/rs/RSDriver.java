db5950057aa8b7851f09475bbee87030e8cecb58
shwetav1311 <cool.shwetav@gmail.com>
2016-09-22 01:58:59 +0530

    implemented create

--- src/com/hbase/rs/RSDriver.java
+++ src/com/hbase/rs/RSDriver.java
@@ -1,18 +1,31 @@
 package com.hbase.rs;
 
-import java.rmi.AlreadyBoundException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.hbase.miscl.HBase.CreateTableRequest;
+import com.hbase.miscl.HBase.CreateTableResponse;
 import com.hbase.miscl.HBaseConstants;
-import com.hdfs.namenode.INameNode;
+import com.hdfs.miscl.Constants;
+import com.hdfs.miscl.PutFile;
 
 public class RSDriver implements IRegionServer {
 	
+	
+	static int id;
+	
+	
 	public static void main(String[] args)
 	{
+		id=Integer.parseInt(args[0]);
+		
 		System.out.println("Region server Binding to Registry...");
 		bindToRegistry();
 	}
@@ -26,17 +39,11 @@ public class RSDriver implements IRegionServer {
 		RSDriver obj = new RSDriver();
 		try {
 			
-			Registry register=LocateRegistry.createRegistry(Registry.REGISTRY_PORT);
-			INameNode stub = (INameNode) UnicastRemoteObject.exportObject(obj,Registry.REGISTRY_PORT);
-			try {
-				register.bind(HBaseConstants.RS_DRIVER, stub);
-				
-				System.out.println("Region server started succesfully");
-			} catch (AlreadyBoundException e) {
-				// TODO Auto-generated catch block
-				System.out.println("Region server failed to bind");
-				e.printStackTrace();
-			}
+			Registry register=LocateRegistry.createRegistry(HBaseConstants.RS_PORT+id);
+			IRegionServer stub = (IRegionServer) UnicastRemoteObject.exportObject(obj,HBaseConstants.RS_PORT+id);
+			register.rebind(HBaseConstants.RS_DRIVER+id, stub);
+			
+			System.out.println("Region server started succesfully");
 			
 		} catch (RemoteException e) {
 			// TODO Auto-generated catch block
@@ -47,7 +54,55 @@ public class RSDriver implements IRegionServer {
 	@Override
 	public byte[] create(byte[] inp) throws RemoteException {
 		// TODO Auto-generated method stub
-		return null;
+		
+		CreateTableResponse.Builder res = CreateTableResponse.newBuilder();
+		res.setStatus(Constants.STATUS_FAILED);
+		
+		try {
+			CreateTableRequest req  = CreateTableRequest.parseFrom(inp);
+			
+			String tableName = req.getTableName();
+			
+			/* write temporary file to be uploaded to hdfs */
+			;
+			String temp = "";
+			
+			for ( String name : req.getColFamiliesList())
+			{
+				temp=temp+name+"\n";
+			}
+			
+			System.out.println(temp);
+			
+			byte data[] = temp.getBytes();
+			
+			FileOutputStream out = new FileOutputStream(tableName);
+			out.write(data);
+			out.close();
+			
+			int status = createTableHDFS(tableName);
+			
+			boolean success = (new File(tableName)).delete();
+		    
+			
+			res.setStatus(status);
+			
+			return res.build().toByteArray();
+			
+			
+		} catch (InvalidProtocolBufferException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return res.build().toByteArray();
 	}
 
 	@Override
@@ -72,5 +127,24 @@ public class RSDriver implements IRegionServer {
 	}
 	
 	
-
+	public static int createTableHDFS(String filename)
+	{
+		PutFile putFile = new PutFile(filename,filename);
+		Thread thread1 = new Thread(putFile);
+		thread1.start();
+		try {
+			thread1.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+			return Constants.STATUS_FAILED;
+		}
+		
+		
+		
+		return putFile.getPutStatus();
+		
+	}
+	
 }
