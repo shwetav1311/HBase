3bedf6c0942b3b2e429803ef718b0b520be4abfb
shwetav1311 <cool.shwetav@gmail.com>
2016-10-21 03:14:44 +0530

    src/com/hbase/miscl/HBaseConstants.java

--- src/com/hbase/miscl/HBaseConstants.java
+++ src/com/hbase/miscl/HBaseConstants.java
@@ -5,5 +5,7 @@ public final class HBaseConstants {
 	public static final String  RS_DRIVER = "RegionServer_";  
 	public static final String  RS_DRIVER_IP = "127.0.0.1";	
 	public static final Integer  RS_PORT = 9000;
+	
+	public static final String  FILE_SEPARATOR = "_";
 
 }
