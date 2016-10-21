c2db9eef81bab5948f1d6df931794b248c4bb120
Sheshadri-1992 <sheshadri.madhu@gmail.com>
2016-10-21 23:41:31 +0530

    Put row

--- src/com/hbase/rs/PutRow.java
+++ src/com/hbase/rs/PutRow.java
@@ -1,7 +1,20 @@
 package com.hbase.rs;
 
+import com.hbase.miscl.HBase.PutRequest;
+
 public class PutRow {
 	
+	MemStore memStore;
 	
+	
+	public PutRow(MemStore memStoreArg) {
+		// TODO Auto-generated constructor stub
+		memStore = memStoreArg;
+	}
+	
+	void appendRow(PutRequest dataIn)
+	{
+		memStore.insertIntoMemStore(dataIn);
+	}
 
 }
