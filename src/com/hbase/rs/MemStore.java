5de2248409375ae419fc237468e812a791c24a3c
shwetav1311 <cool.shwetav@gmail.com>
2016-11-12 23:21:10 +0530

    commit

--- src/com/hbase/rs/MemStore.java
+++ src/com/hbase/rs/MemStore.java
@@ -2,6 +2,7 @@ package com.hbase.rs;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.TreeMap;
 
 import com.hbase.miscl.HBase.Cell;
@@ -12,15 +13,16 @@ import com.hbase.miscl.HBase.PutRequest;
 public class MemStore {
 	
 	
-	public TreeMap<String, TreeMap<String, TreeMap<String, ArrayList<Cell> > > > memStore;
-	public TreeMap<String, TreeMap<String, TreeMap<String, ArrayList<Cell> > > > tempStore; 
+	public TreeMap<String, TreeMap<String, TreeMap<String, List<Cell> > > > memStore;
+	public TreeMap<String, TreeMap<String, TreeMap<String, List<Cell> > > > tempStore; 
 	public static String tableName;
 	public String startKey;
 	public String endKey;
+	public int count = 0;
 	
 	public MemStore(String tblName,String sKey,String eKey)
 	{
-		memStore = new TreeMap<String, TreeMap<String, TreeMap<String, ArrayList<Cell> > > >();		
+		memStore = new TreeMap<String, TreeMap<String, TreeMap<String, List<Cell> > > >();		
 		tableName  = tblName;
 		startKey = sKey;
 		endKey = eKey;
@@ -28,28 +30,30 @@ public class MemStore {
 	
 	void insertIntoMemStore(PutRequest dataIn)
 	{
+		
+		System.out.println("recaching insert memstore");
 		String rowKey = dataIn.getRowkey();
 		if(memStore.containsKey(rowKey)==false)
 		{
 			/** Press ctrl-1 to get quick fix as options **/
-			ArrayList<ColumnFamily> myColFamily = (ArrayList<ColumnFamily>) dataIn.getColFamilyList();
+			List<ColumnFamily> myColFamily = (List<ColumnFamily>) dataIn.getColFamilyList();
 			
 			/**Level 2 Map **/
-			TreeMap<String, TreeMap<String, ArrayList<Cell> > > colFamilyMap = new TreeMap<String, TreeMap<String, ArrayList<Cell> > >();  
+			TreeMap<String, TreeMap<String, List<Cell> > > colFamilyMap = new TreeMap<String, TreeMap<String, List<Cell> > >();  
 			for (Iterator<ColumnFamily> iterator = myColFamily.iterator(); iterator.hasNext();) {
 				
 				ColumnFamily columnFamily = (ColumnFamily) iterator.next();
 				
 				String colFamilyName = columnFamily.getName();
-				ArrayList<Column> myColumns = (ArrayList<Column>) columnFamily.getColumnsList();
+				List<Column> myColumns = (List<Column>) columnFamily.getColumnsList();
 				
 				/** Final level Map **/
-				TreeMap<String, ArrayList<Cell> > cellListMap = new TreeMap<String, ArrayList<Cell> >(); 
+				TreeMap<String, List<Cell> > cellListMap = new TreeMap<String, List<Cell> >(); 
 				for (Iterator<Column> iterator2 = myColumns.iterator(); iterator2.hasNext();) {
 					Column column = (Column) iterator2.next();
 					
 					String columnName = column.getColName();
-					ArrayList<Cell> myCells =  new ArrayList<Cell>(); //) column.getCellsList();
+					List<Cell> myCells =  new ArrayList<Cell>(); //) column.getCellsList();
 					myCells.addAll(column.getCellsList()); //check this if there is any problem
 					cellListMap.put(columnName, myCells); //Final level map added
 					
@@ -62,15 +66,15 @@ public class MemStore {
 		}
 		else
 		{
-			ArrayList<ColumnFamily> myColFamily = (ArrayList<ColumnFamily>) dataIn.getColFamilyList();
+			List<ColumnFamily> myColFamily = (List<ColumnFamily>) dataIn.getColFamilyList();
 			/**Level 2 Map **/
-			TreeMap<String, TreeMap<String, ArrayList<Cell> > > colFamilyMap = memStore.get(rowKey);  
+			TreeMap<String, TreeMap<String, List<Cell> > > colFamilyMap = memStore.get(rowKey);  
 			for (Iterator<ColumnFamily> iterator = myColFamily.iterator(); iterator.hasNext();) {
 				
 				ColumnFamily columnFamily = (ColumnFamily) iterator.next();
 				
 				String colFamilyName = columnFamily.getName();
-				ArrayList<Column> myColumns = (ArrayList<Column>) columnFamily.getColumnsList();
+				List<Column> myColumns = (List<Column>) columnFamily.getColumnsList();
 				
 				if(colFamilyMap.containsKey(colFamilyName)==false)
 				{
@@ -79,17 +83,17 @@ public class MemStore {
 				else /** column family is present **/
 				{
 					/** Final level Map **/
-					TreeMap<String, ArrayList<Cell> > cellListMap = colFamilyMap.get(columnFamily); 
+					TreeMap<String, List<Cell> > cellListMap = colFamilyMap.get(colFamilyName); 
 					
 					for (Iterator<Column> iterator2 = myColumns.iterator(); iterator2.hasNext();) {
 						Column column = (Column) iterator2.next();
 						
 						String columnName = column.getColName();
-						ArrayList<Cell> myCell = (ArrayList<Cell>) column.getCellsList();
+						List<Cell> myCell = (List<Cell>) column.getCellsList();
 						
 						if(cellListMap.containsKey(columnName)==false)/** column key is absent **/
 						{
-							ArrayList<Cell> myCells =  new ArrayList<Cell>(); //) column.getCellsList();
+							List<Cell> myCells =  new ArrayList<Cell>(); //) column.getCellsList();
 							myCells.addAll(column.getCellsList()); //check this if there is any problem
 							cellListMap.put(columnName, myCells); //Final level map added
 						}
@@ -102,11 +106,11 @@ public class MemStore {
 			}
 			
 		}
-		
-		if(isMemStoreFull(4))
+		count++;
+		if(isMemStoreFull(count))
 		{
 			tempStore = memStore;
-			memStore = new TreeMap<String, TreeMap<String, TreeMap<String, ArrayList<Cell> > > >();
+			memStore = new TreeMap<String, TreeMap<String, TreeMap<String, List<Cell> > > >();
 			memStore.clear();
 			writeToHFile();
 		}
@@ -120,20 +124,20 @@ public class MemStore {
 			myObj.write(tableName,startKey,endKey);
 	}
 
-	private static TreeMap<String, TreeMap<String, ArrayList<Cell>>> insertNewColumnFamily(ColumnFamily columnFamily,TreeMap<String, TreeMap<String, ArrayList<Cell> > > colFamilyMap ) {
+	private static TreeMap<String, TreeMap<String, List<Cell>>> insertNewColumnFamily(ColumnFamily columnFamily,TreeMap<String, TreeMap<String, List<Cell> > > colFamilyMap ) {
 		// TODO Auto-generated method stub
 		
 		
 		String colFamilyName = columnFamily.getName();
-		ArrayList<Column> myColumns = (ArrayList<Column>) columnFamily.getColumnsList();
+		List<Column> myColumns = (List<Column>) columnFamily.getColumnsList();
 		
 		/** Final level Map **/
-		TreeMap<String, ArrayList<Cell> > cellListMap = new TreeMap<String, ArrayList<Cell> >(); 
+		TreeMap<String, List<Cell> > cellListMap = new TreeMap<String, List<Cell> >(); 
 		for (Iterator<Column> iterator2 = myColumns.iterator(); iterator2.hasNext();) {
 			Column column = (Column) iterator2.next();
 			
 			String columnName = column.getColName();
-			ArrayList<Cell> myCells =  new ArrayList<Cell>(); //) column.getCellsList();
+			List<Cell> myCells =  new ArrayList<Cell>(); //) column.getCellsList();
 			
 			myCells.addAll(column.getCellsList()); // get cells in not a list, just a single value
 			cellListMap.put(columnName, myCells); //Final level map added
@@ -169,7 +173,7 @@ public class MemStore {
 		
 	}
 	
-	ArrayList<com.hbase.miscl.HBase.ColumnFamily> searchMemStore()  // when get method searches in memstore
+	List<com.hbase.miscl.HBase.ColumnFamily> searchMemStore()  // when get method searches in memstore
 	{
 		return null;
 		
