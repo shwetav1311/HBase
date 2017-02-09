918487f710517c9ca1b20ecd71ceeb6435313412
shwetav1311 <cool.shwetav@gmail.com>
2017-02-09 20:00:37 +0530

    backing up

---
+++ src/com/hdfs/bkp/GetFile.java
@@ -0,0 +1,223 @@
+package com.hdfs.bkp;
+
+import java.nio.charset.StandardCharsets;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.List;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.hdfs.datanode.IDataNode;
+import com.hdfs.miscl.Constants;
+import com.hdfs.miscl.FileWriterClass;
+import com.hdfs.miscl.Hdfs.BlockLocationRequest;
+import com.hdfs.miscl.Hdfs.BlockLocationResponse;
+import com.hdfs.miscl.Hdfs.BlockLocations;
+import com.hdfs.miscl.Hdfs.DataNodeLocation;
+import com.hdfs.miscl.Hdfs.OpenFileRequest;
+import com.hdfs.miscl.Hdfs.OpenFileResponse;
+import com.hdfs.miscl.Hdfs.ReadBlockRequest;
+import com.hdfs.miscl.Hdfs.ReadBlockResponse;
+import com.hdfs.namenode.INameNode;
+
+public class GetFile implements Runnable{
+
+	
+	String fileName;
+	String localOutFile;
+	int status;
+	
+	
+	public GetFile(String fileName,String localOut) {
+		// TODO Auto-generated constructor stub
+		
+		this.fileName = fileName;
+		this.localOutFile = localOut;
+	}
+	
+	 public int getStatus() {
+		return status;
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+		this.status=Constants.STATUS_SUCCESS;
+		OpenFileRequest.Builder openFileReqObj = OpenFileRequest.newBuilder();
+		openFileReqObj.setFileName(fileName);		
+		openFileReqObj.setForRead(true);
+		
+		
+		FileWriterClass fileWriteObj = new FileWriterClass(localOutFile);
+		fileWriteObj.createFile();
+		
+		byte[] responseArray;
+		
+		try {
+			//here obtain the IP, port of the namenode, so that we can register to the services 
+			//provided by the namenode
+			Registry registry=LocateRegistry.getRegistry(Constants.NAME_NODE_IP,Registry.REGISTRY_PORT);
+			INameNode nameStub;
+			nameStub=(INameNode) registry.lookup(Constants.NAME_NODE);
+			responseArray = nameStub.openFile(openFileReqObj.build().toByteArray());
+			
+			try {
+				//Get is the read functionality from the HDFS file system
+				OpenFileResponse responseObj = OpenFileResponse.parseFrom(responseArray);
+				if(responseObj.getStatus()==Constants.STATUS_NOT_FOUND)
+				{
+					System.out.println("File not found fatal error");
+					this.status=Constants.STATUS_NOT_FOUND;
+					return;
+				}
+				
+				//receives all the block numbers associated with a given file
+				List<String> blockNums = responseObj.getBlockNumsList();
+				BlockLocationRequest.Builder blockLocReqObj = BlockLocationRequest.newBuilder();
+				
+				/**Now, all blocks related to that file is present with us  
+				 * in blockNums
+				 * perform Read Block Request  from all the blockNums**/
+				
+				blockLocReqObj.addAllBlockNums(blockNums);
+												
+				try {
+					responseArray = nameStub.getBlockLocations(blockLocReqObj.build().toByteArray());
+				} catch (RemoteException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				
+				
+				
+				BlockLocationResponse blockLocResObj = BlockLocationResponse.parseFrom(responseArray);
+				
+				/**block location response returns all the block locations of each of the 
+				 * blocks that were sent by blockNums
+				 */
+				
+				if(blockLocResObj.getStatus()==Constants.STATUS_FAILED)
+				{
+					System.out.println("Fatal error!");
+					this.status=Constants.STATUS_FAILED;
+					return;
+				}
+				
+				/** We might have many blocks per file, so we get the datanode locations of each
+				 * block as a response to the request nameStub.getBlockLocations
+				 */
+				List<BlockLocations> blockLocations =  blockLocResObj.getBlockLocationsList();
+				
+				
+				for(int i=0;i<blockLocations.size();i++)
+				{
+					BlockLocations thisBlock = blockLocations.get(i);
+					
+					String blockNumber = thisBlock.getBlockNumber();					
+					List<DataNodeLocation> dataNodes = thisBlock.getLocationsList();
+					
+					if(dataNodes==null || dataNodes.size()==0)
+					{
+						System.out.println("All nodes are down :( ");
+						this.status=Constants.STATUS_FAILED;
+						return;
+					}
+					
+					int dataNodeCounter=0;
+					
+					DataNodeLocation thisDataNode = null;//dataNodes.get(dataNodeCounter);					
+					String ip;// = thisDataNode.getIp();
+					int port ; //= thisDataNode.getPort();
+					
+					
+					IDataNode dataStub=null;
+					
+					boolean gotDataNodeFlag=false;
+					
+					/**
+					 * The following do while loop tries to retrieve the data block that we are looking
+					 * for, in case it is not present in one of the data locations that we first queried for.
+					 * it goes to check with another data node that was sent in the response,
+					 * this continues until we find the block or all data node locations exhaust
+					 */
+					do
+					{
+						try
+						{
+							thisDataNode = dataNodes.get(dataNodeCounter);
+							ip = thisDataNode.getIp();
+							port = thisDataNode.getPort();
+														
+							Registry registry2=LocateRegistry.getRegistry(ip,port);					
+							dataStub = (IDataNode) registry2.lookup(Constants.DATA_NODE_ID);
+							gotDataNodeFlag=true;
+						}
+						catch (RemoteException e) {
+							
+							gotDataNodeFlag=false;
+//							System.out.println("Remote Exception");
+							dataNodeCounter++;
+						} 
+					}					
+					while(gotDataNodeFlag==false && dataNodeCounter<dataNodes.size());
+					
+					/**This is an indication to say that even after checking all the datanodes
+					 * for that particular block we couldn't get the block since all the nodes were down
+					 * so we exit ( we may discuss and change it)
+					 */
+					if(dataNodeCounter == dataNodes.size())
+					{
+						System.out.println("All data nodes are down :( ");
+						this.status=Constants.STATUS_FAILED;
+						return;
+					}
+					
+
+					/**Construct Read block request **/
+					ReadBlockRequest.Builder readBlockReqObj = ReadBlockRequest.newBuilder();
+					readBlockReqObj.setBlockNumber(blockNumber);
+					
+					/**Read block request call **/
+					
+					responseArray = dataStub.readBlock(readBlockReqObj.build().toByteArray());
+					ReadBlockResponse readBlockResObj = ReadBlockResponse.parseFrom(responseArray);
+					
+					if(readBlockResObj.getStatus()==Constants.STATUS_FAILED)
+					{
+						System.out.println("In method openFileGet(), readError");
+						this.status=Constants.STATUS_FAILED;
+						return;
+					}
+					
+					responseArray = readBlockResObj.getData(0).toByteArray();	
+//					System.out.println("Response array size"+responseArray.length);
+					
+					String str = new String(responseArray, StandardCharsets.UTF_8);						
+					//fileWriteObj.writeonly(str);
+					fileWriteObj.writeBytes(responseArray);
+
+				}
+				
+				
+			} catch (InvalidProtocolBufferException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				this.status=Constants.STATUS_FAILED;
+				
+			}
+		} catch (NotBoundException e) {
+			System.out.println("Exception caught: NotBoundException ");			
+			this.status=Constants.STATUS_FAILED;
+			
+		} catch (RemoteException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
+		fileWriteObj.closeFile();
+		
+		
+	}
+
+}
---
+++ src/com/hdfs/bkp/PutFile.java
@@ -0,0 +1,294 @@
+package com.hdfs.bkp;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.List;
+
+import com.google.protobuf.ByteString;
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.hdfs.datanode.IDataNode;
+import com.hdfs.miscl.Constants;
+import com.hdfs.miscl.Hdfs.AssignBlockRequest;
+import com.hdfs.miscl.Hdfs.AssignBlockResponse;
+import com.hdfs.miscl.Hdfs.BlockLocations;
+import com.hdfs.miscl.Hdfs.CloseFileRequest;
+import com.hdfs.miscl.Hdfs.CloseFileResponse;
+import com.hdfs.miscl.Hdfs.DataNodeLocation;
+import com.hdfs.miscl.Hdfs.OpenFileRequest;
+import com.hdfs.miscl.Hdfs.OpenFileResponse;
+import com.hdfs.miscl.Hdfs.WriteBlockRequest;
+import com.hdfs.namenode.INameNode;
+
+public class PutFile implements Runnable {
+
+	public static String fileName;
+	public static long FILESIZE;
+	public FileInputStream fis;
+	public String threadName;
+	private Thread t;
+	
+	public int status;
+	
+	public PutFile(String threadNameArgs,String fileNameArgs) {
+		super();
+		threadName = threadNameArgs;
+		fileName = fileNameArgs;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		openFilePut(); //PUT to HDFS
+	}
+
+	public int getPutStatus() {
+		
+		return status;
+	}
+	
+	private void openFilePut() {
+		// TODO Auto-generated method stub
+		
+		int fileHandle;
+		this.status=Constants.STATUS_SUCCESS;
+		byte[] responseArray;
+		
+		OpenFileRequest.Builder openFileReqObj = OpenFileRequest.newBuilder();
+		openFileReqObj.setFileName(fileName);
+		openFileReqObj.setForRead(false);		
+
+		
+		try 
+		{			
+			Registry registry=LocateRegistry.getRegistry(Constants.NAME_NODE_IP,Registry.REGISTRY_PORT);
+			INameNode nameStub;
+			int status;
+			
+				try 
+				{
+					nameStub = (INameNode) registry.lookup(Constants.NAME_NODE);
+					responseArray = nameStub.openFile(openFileReqObj.build().toByteArray());
+					
+					/**The response Array will contain the FileHandle status and the block numbers **/
+					
+					OpenFileResponse responseObj = OpenFileResponse.parseFrom(responseArray);
+					
+					fileHandle = responseObj.getHandle();
+					System.out.println("The file handle is "+fileHandle);
+					
+					status = responseObj.getStatus();
+					if(status==Constants.STATUS_FAILED )//status failed change it
+					{
+						System.out.println("Fatal Error!");
+//						System.exit(0);
+					}
+					else if(status==Constants.STATUS_NOT_FOUND)
+					{
+						System.out.println("Duplicate File");
+//						System.exit(0);
+					}
+					
+					AssignBlockRequest.Builder assgnBlockReqObj = AssignBlockRequest.newBuilder(); 
+					
+					
+					/**required variables **/
+
+					
+					int offset=0;
+					
+					/**calculate block size **/
+					int no_of_blocks=getNumberOfBlocks();					
+					try {
+						/**open the input stream **/
+						fis = new FileInputStream(fileName);
+					} catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+
+//					System.out.println("No of blocks are "+no_of_blocks);
+					if(no_of_blocks==0)
+						no_of_blocks=1;
+					
+					/**FOR LOOP STARTS HERE **/
+					for(int i=0;i<no_of_blocks;i++)
+					{
+						WriteBlockRequest.Builder writeBlockObj = WriteBlockRequest.newBuilder();
+						AssignBlockResponse assignResponseObj ;
+						BlockLocations blkLocation ;
+						List<DataNodeLocation> dataNodeLocations;
+						DataNodeLocation dataNode;
+						/**need to call assign block and write blocks **/
+						
+						assgnBlockReqObj.setHandle(fileHandle);
+						
+						/**Calling assign block **/
+						responseArray = nameStub.assignBlock(assgnBlockReqObj.build().toByteArray());
+						
+						assignResponseObj = AssignBlockResponse.parseFrom(responseArray);
+						
+						status = assignResponseObj.getStatus();
+						if(status==Constants.STATUS_FAILED)
+						{
+							System.out.println("Fatal Error!");
+//							System.exit(0);
+						}
+						
+						blkLocation = assignResponseObj.getNewBlock();
+						
+						String blockNumber = blkLocation.getBlockNumber();
+						System.out.println("Block number retured is "+blockNumber);
+						
+						dataNodeLocations = blkLocation.getLocationsList();
+						
+						dataNode = dataNodeLocations.get(0);
+//						dataNodeLocations.remove(0);
+						
+						
+						Registry registry2=LocateRegistry.getRegistry(dataNode.getIp(),dataNode.getPort());
+
+						System.out.println(dataNode);
+						IDataNode dataStub = (IDataNode) registry2.lookup(Constants.DATA_NODE_ID);
+//						dataStub.readBlock(null);
+						
+//						System.out.println("Control enters here");
+						/**read 32MB from file, send it as bytes, this fills in the byteArray**/
+						
+						byte[] byteArray = read32MBfromFile(offset);
+						offset=offset+(int)Constants.BLOCK_SIZE;
+						
+						System.out.println("Byte array"+byteArray.length);
+						
+						writeBlockObj.addData(ByteString.copyFrom(byteArray));
+						writeBlockObj.setBlockInfo(blkLocation);
+						
+						dataStub.writeBlock(writeBlockObj.build().toByteArray());
+												
+					}
+					
+					CloseFileRequest.Builder closeFileObj = CloseFileRequest.newBuilder();
+					closeFileObj.setHandle(fileHandle);
+					closeFileObj.setDecision(Constants.STATUS_NOT_FOUND);
+					
+					byte[] receivedArray = nameStub.closeFile(closeFileObj.build().toByteArray());
+					CloseFileResponse closeResObj = CloseFileResponse.parseFrom(receivedArray);
+					if(closeResObj.getStatus()==Constants.STATUS_FAILED)
+					{
+						System.out.println("Close File response Status Failed");
+//						System.exit(0);
+					}
+					
+					try {
+						/**Close the input Stream **/
+						fis.close();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+				}
+				catch (NotBoundException | InvalidProtocolBufferException e) {
+					// TODO Auto-generated catch block
+					System.out.println("Could not find NameNode");
+					e.printStackTrace();
+				}
+				
+			
+		}catch (RemoteException e) {
+			// TODO Auto-generated catch block
+				e.printStackTrace();
+		}		
+		
+		
+	}
+	
+
+	public static int getNumberOfBlocks()
+	{
+		File inputFile = new File(fileName);
+		if(!inputFile.exists())
+		{
+			System.out.println("File Does not exist");
+//			System.exit(0);
+		}
+		
+		long fileSize = inputFile.length();
+		FILESIZE=inputFile.length();
+		
+		
+		double noOfBlocks = Math.ceil((double)fileSize*1.0/(double)Constants.BLOCK_SIZE*1.0);
+		
+		System.out.println("The length of the file is "+fileSize+ " Number of blocks are "+(int)noOfBlocks);
+		
+		return (int)noOfBlocks;
+	}
+	
+	/**Read 32MB size of data from the provided input file **/
+	public static byte[] read32MBfromFile(int offset)
+	{
+		
+		System.out.println("offset is "+offset);
+
+		
+		FileInputStream breader = null;
+		try {
+			breader = new FileInputStream(fileName);
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+
+		
+		
+		int bytesToBeRead = (int)Constants.BLOCK_SIZE;
+		
+		int limit =offset+(int)Constants.BLOCK_SIZE; 
+		
+		if(limit >= (int) FILESIZE)
+		{
+			bytesToBeRead = (int)FILESIZE - offset;
+		}
+		else
+		{
+			bytesToBeRead = (int)Constants.BLOCK_SIZE;			
+		}
+		
+		
+		
+		byte[] newCharArray = new byte[bytesToBeRead];
+		
+		try {
+//			breader.skip(offset);
+			breader.read(newCharArray, 0, bytesToBeRead);
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+		try {
+			breader.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+//		System.out.println("The new char array is "+newCharArray.length);
+		return newCharArray;
+		
+	}
+	
+	
+
+	
+	
+	
+	
+}
