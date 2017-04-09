f2927b03430c02915fd2701405c435e4d76c1fbe
shwetav1311 <cool.shwetav@gmail.com>
2017-04-09 17:37:29 +0530

    Integration

--- client.sh
+++ client.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [ $1 == '-c' ]	
 then
-	javac -sourcepath src -d bin src/**/**/**/*.java 
+	javac -cp lib/zookeeper-3.4.9.jar:lib/slf4j-api-1.7.25.jar:lib/slf4j-simple-1.7.25.jar -sourcepath src -d bin src/**/**/**/*.java
 	echo "Compilation success"
 else
-	java -cp bin: com.hbase.client.ClientDriver $@
+	java -cp $PWD/lib/zookeeper-3.4.9.jar:$PWD/lib/slf4j-api-1.7.25.jar:$PWD/lib/slf4j-simple-1.7.25.jar:bin: com.hbase.client.ClientDriver $@
 fi
 
